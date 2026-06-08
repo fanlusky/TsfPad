a8e9de11e56c30f623106834a8894475a5c5ef15
fanlusky <1730976608@qq.com>
2026-06-08 08:47:04 +0800

    chore

--- src/DisplayAttribute.cpp
+++ src/DisplayAttribute.cpp
@@ -1,6 +1,73 @@
 #include "DisplayAttribute.h"
+#include <string>
+#include <Windows.h>
+#include <msctf.h>
+#include <fmt/xchar.h>
 
 ITfDisplayAttributeMgr *g_pdam = NULL;
+
+std::wstring ColorTypeToStr(TF_DA_COLORTYPE type)
+{
+    std::string res;
+    switch (type)
+    {
+    case TF_CT_NONE:
+        res = "None";
+        break;
+    case TF_CT_SYSCOLOR:
+        res = "SysColor";
+        break;
+    case TF_CT_COLORREF:
+        res = "ColorRef";
+        break;
+    default:
+        res = "Unknown";
+    }
+    return std::wstring(res.begin(), res.end());
+}
+
+std::wstring UnderlineStyleToStr(TF_DA_LINESTYLE style)
+{
+    std::string res;
+    switch (style)
+    {
+    case TF_LS_NONE:
+        res = "None";
+        break;
+    case TF_LS_SOLID:
+        res = "Solid";
+        break;
+    case TF_LS_DOT:
+        res = "Dot";
+        break;
+    case TF_LS_DASH:
+        res = "Dash";
+        break;
+    case TF_LS_SQUIGGLE:
+        res = "Squiggle";
+        break;
+    default:
+        res = "Unknown";
+    }
+    return std::wstring(res.begin(), res.end());
+}
+
+void PrintColor(const TF_DA_COLOR &color)
+{
+    OutputDebugString(fmt::format(ColorTypeToStr(color.type)).c_str());
+    if (color.type == TF_CT_COLORREF)
+    {
+        COLORREF cr = color.cr;
+    }
+    else if (color.type == TF_CT_SYSCOLOR)
+    {
+    }
+}
+
+void PrintDisplayAttribute(const TF_DISPLAYATTRIBUTE &da)
+{
+    OutputDebugString(fmt::format(UnderlineStyleToStr(da.lsStyle)).c_str());
+}
 
 CDispAttrProps *GetDispAttrProps()
 {
@@ -50,6 +117,8 @@ CDispAttrProps *GetDispAttrProps()
 HRESULT InitDisplayAttrbute()
 {
 
+    OutputDebugString(fmt::format(L"InitDisplayAttrbute tsfpad").c_str());
+
     if (g_pdam)
         g_pdam->Release();
 
@@ -58,7 +127,13 @@ HRESULT InitDisplayAttrbute()
     if (FAILED(CoCreateInstance(CLSID_TF_DisplayAttributeMgr, NULL, CLSCTX_INPROC_SERVER, IID_ITfDisplayAttributeMgr,
                                 (void **)&g_pdam)))
     {
+        OutputDebugString(fmt::format(L"CoCreateInstance failed").c_str());
         return E_FAIL;
+    }
+
+    if (g_pdam == NULL)
+    {
+        OutputDebugString(fmt::format(L"CoCreateInstance failed02").c_str());
     }
 
     return S_OK;
@@ -88,8 +163,13 @@ HRESULT UninitDisplayAttrbute()
 //
 //----------------------------------------------------------------------------
 
-HRESULT GetDisplayAttributeTrackPropertyRange(TfEditCookie ec, ITfContext *pic, ITfRange *pRange,
-                                              ITfReadOnlyProperty **ppProp, CDispAttrProps *pDispAttrProps)
+HRESULT GetDisplayAttributeTrackPropertyRange( //
+    TfEditCookie ec,                           //
+    ITfContext *pic,                           //
+    ITfRange *pRange,                          //
+    ITfReadOnlyProperty **ppProp,              //
+    CDispAttrProps *pDispAttrProps             //
+)
 {
     ITfReadOnlyProperty *pProp = NULL;
     HRESULT hr = E_FAIL;
@@ -157,8 +237,10 @@ HRESULT GetDisplayAttributeData(TfEditCookie ec, ITfReadOnlyProperty *pProp, ITf
     hr = S_FALSE;
     if (SUCCEEDED(pProp->GetValue(ec, pRange, &var)))
     {
+        OutputDebugString(fmt::format(L"GetDisplayAttributeData01").c_str());
         if (SUCCEEDED(var.punkVal->QueryInterface(IID_IEnumTfPropertyValue, (void **)&pEnumPropertyVal)))
         {
+            OutputDebugString(fmt::format(L"GetDisplayAttributeData02").c_str());
             while (pEnumPropertyVal->Next(1, &tfPropVal, NULL) == S_OK)
             {
                 if (tfPropVal.varValue.vt == VT_EMPTY)
@@ -168,8 +250,14 @@ HRESULT GetDisplayAttributeData(TfEditCookie ec, ITfReadOnlyProperty *pProp, ITf
 
                 pcat->GetGUID(gaVal, &guid);
 
+                if ((g_pdam != NULL))
+                {
+                    OutputDebugString(fmt::format(L"GetDisplayAttributeData2.5").c_str());
+                }
+
                 if ((g_pdam != NULL) && SUCCEEDED(g_pdam->GetDisplayAttributeInfo(guid, &pDAI, NULL)))
                 {
+                    OutputDebugString(fmt::format(L"GetDisplayAttributeData03").c_str());
                     //
                     // Issue: for simple apps.
                     //
@@ -180,6 +268,7 @@ HRESULT GetDisplayAttributeData(TfEditCookie ec, ITfReadOnlyProperty *pProp, ITf
                     if (pda)
                     {
                         pDAI->GetAttributeInfo(pda);
+                        PrintDisplayAttribute(*pda);
                     }
 
                     if (pguid)
